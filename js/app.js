6f028360ea5affe7fd7861f04b66bec52325521e
Jorge Faundez <jaff.faundez@gmail.com>
2022-07-06 19:50:16 -0400

    Se agrega libreria sweetalert, filtro de busqueda, vaciar carrito y suma cantidad en una casilla

--- js/app.js
+++ js/app.js
@@ -1,31 +1,39 @@
 
+
 const  contenedorPro = document.getElementById("contenedorPro")
 
 const  carritoContenedor = document.getElementById("carrito-contenedor")
 
 const  totalCarrito = document.getElementById("total")
 
+const botonBuscarProductos = document.querySelector("#buscarPro")
+
+const formularioBuscar = document.querySelector("#buscando")
 
 const cantidadCarrito = document.getElementById("contador-carrito")
+
+const encontrado = document.querySelector("#encontrado")
+
+
 
 let carrito = [ ]
 
 document.addEventListener("DOMContentLoaded", () => {
     if (localStorage.getItem("carrito")){
-      carrito = JSON.parse(localStorage.getItem("carrito")) 
+      carrito = JSON.parse(localStorage.getItem("carrito"))
       actCarrito()
     }
-})
+}) 
 
   producto.forEach(productos => {
     const div = document.createElement("div")
     div.classList.add("card")
-    div.innerHTML += ` <div class="card">
+    div.innerHTML += `  <div class="card">
                                      <img src="${productos.img}" class="card-img-top" alt="alimento-mascotas">
                                     <div class="card-body">
                                     <h5 class="card-title">${productos.nombre}</h5>
                                     <p class="card-text">${productos.descripcion}</p>
-                                    <p class="card-text">Precio $: ${productos.precio}</p>
+                                    <p class="card-text">Precio: ${productos.precio}</p>
                                     <a href="#" class="btn btn-primary" id=comprar${productos.id}>Comprar</a>
                                     </div>
                                     </div>`
@@ -44,13 +52,42 @@ document.addEventListener("DOMContentLoaded", () => {
     })
 
 const  agregarAlCarrito = (prodId) =>{
-    const   prods = producto.find((prod) => prod.id === prodId)
-    carrito.push(prods)
+    const estaEnCarrito = carrito.some(prod => prod.id === prodId)
+
+    if (estaEnCarrito) {
+      const prods = carrito.map (prod =>{
+        if (prod.id === prodId) {
+          prod.cantidad++
+        }
+      })
+    }else{
+      const   prods = producto.find((prod) => prod.id === prodId)
+          carrito.push(prods)
+  }
     actCarrito()
-    console.log(carrito)
 }
 
+const filtrarProductos = () => {
+  encontrado.innerHTML= ""
+  const buscando = formularioBuscar.value.toLowerCase()
+  for (let productos of producto) {
+      let item = productos.nombre.toLowerCase()
+      if (item.indexOf (buscando) !== -1) {
+        encontrado.innerHTML += `
+                <li><p> ${productos.nombre} ${productos.descripcion}</p></li>     
+        `
+      }
+  }
+  if (encontrado.innerHTML === "") {
+    encontrado.innerHTML += ` <li>No se encuentra el producto</li>
+    `
+  }
+}
 
+// botonBuscarProductos.addEventListener("click", filtrarProductos)
+formularioBuscar.addEventListener("keyup", filtrarProductos)
+
+//filtrarProductos()
 
 
 
@@ -60,18 +97,49 @@ const quitarDelCarrito = (prodId) =>{
   const indice = carrito.indexOf(prods)
   carrito.splice(indice, 1)
   actCarrito()
+  sweetAlert (`${prods.nombre} ${prods.descripcion} Se elimino del carrito`)
 }
+
+const sweetAlert = (nombre, descripcion) =>{
+  Swal.fire({
+    title: nombre,
+    text: descripcion,
+    icon: 'warning',
+    toast:true,
+    timer: 4000,
+    position: 'top'
+  })
+}
+
+const vaciarCarrito = document.getElementById("vaciar")
+
+vaciarCarrito.addEventListener("click", ()=> {
+  carrito.length = 0
+  alertVacio("Tu carrito esta vacio")
+  actCarrito()
+})
+
+const alertVacio = (hecho) =>{
+  Swal.fire({
+    title:hecho,
+    icon: 'info',
+    toast:true,
+    timer: 3000,
+    position:'top'
+  })
+}
+
 
 const actCarrito = ()=>{
 
-    carritoContenedor.innerHTML=""
-
+  carritoContenedor.innerHTML = ""
+    
     carrito.forEach((producto) =>{
         const div = document.createElement("div")
         div.className = ("enCarrito")
         div.innerHTML =`<h6>${producto.nombre}</h6>
                                     <p>${producto.descripcion}</p>
-                                    <p>${producto.precio}</p>
+                                    <p> Precio: ${producto.precio}</p>
                                     <p>Cantidad: <span>${producto.cantidad}</span></p>
                                     <button onclick = "quitarDelCarrito(${producto.id})" class="boton_eliminar"><i class="fa-solid fa-trash-can"></i></button>`
 
@@ -84,8 +152,7 @@ const actCarrito = ()=>{
 
          cantidadCarrito.innerText = carrito.length
          
-         totalCarrito.innerText = carrito.reduce((acc, producto) => acc + producto.precio, 0)
+         totalCarrito.innerText = carrito.reduce((acc, producto,) => acc + producto.precio * producto.cantidad, 0)
          
-
          
 }
---
+++ js/formulario.js
@@ -0,0 +1,60 @@
+const formulario = document.querySelector("#formulario")
+const inputNombre = document.querySelector("#inputNombre")
+const inputApellido = document.querySelector("#inputApellido")
+const inputEmail = document.querySelector("#inputEmail")
+const btnEnviar = document.querySelector("#btnEnviar")
+const inp = document.querySelectorAll("input")
+
+
+const focoEnInput = () =>{
+    const inp = document.querySelectorAll("input")
+    for (let inpFondo of inp){
+        if (inpFondo.type != "submit"){
+            inpFondo.addEventListener("focus", () => inpFondo.className = "inputColor")
+            inpFondo.addEventListener("blur", () => inpFondo.className = "")
+
+        }
+    }
+}
+
+document.addEventListener("submit", (e) =>{
+    e.preventDefault()
+    saveDateUsr()
+    alertInput("Datos guardados")
+})
+
+const alertInput = (hecho) =>{
+    Swal.fire({
+      title:hecho,
+      icon: 'success',
+      toast:true,
+      timer: 1000,
+      position:'top'
+    })
+  }
+
+
+
+function saveDateUsr() {
+    const dateUsr = {
+        nombre: inputNombre.value,
+        apellido: inputApellido.value,
+        email: inputEmail.value
+    }
+
+    let aStr = JSON.stringify(dateUsr)
+    localStorage.setItem("dateUsr", aStr )
+}
+function recuperarDatosUsr (){
+    if (localStorage.getItem("dateUsr")){
+        const dateUsr = JSON.parse(localStorage.getItem("dateUsr"))
+            inputNombre.value = dateUsr.nombre
+            inputApellido.value = dateUsr.apellido
+            inputEmail.value = dateUsr.email
+    }
+        
+}
+
+
+
+
